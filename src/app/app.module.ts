15fc042812646a25c3f200d7212f836129d7b93b
Biswajit Ganguly <biswajit29nov@gmail.com>
2019-08-20 02:24:35 +0530

    Add Full Calender

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FullCalendarModule } from '@fullcalendar/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FullCalendarModule,
     AppRoutingModule
   ],
   providers: [],
